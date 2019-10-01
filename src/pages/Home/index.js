300977ca00ccb2596108b3436e0b322317ad1878
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-30 21:58:12 -0300

    chore: home stylization

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+import macbookpro13 from '../../assets/images/macbookpro13.jpg';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <picture>
+          <img src={macbookpro13} alt="Macbook Pro 13" />
+        </picture>
+        <strong>Macbook Pro 13 - best to developer</strong>
+        <span>R$11.000,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
