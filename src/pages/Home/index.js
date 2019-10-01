a32f3c81823d740d7289c4b929c7dba9e4d6ab54
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-30 23:26:34 -0300

    chore: searching API products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,90 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 import macbookpro13 from '../../assets/images/macbookpro13.jpg';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <picture>
-          <img src={macbookpro13} alt="Macbook Pro 13" />
-        </picture>
-        <strong>Macbook Pro 13 - best to developer</strong>
-        <span>R$11.000,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <picture>
+              <img src={product.image} alt={product.title} />
+            </picture>
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Add to cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
